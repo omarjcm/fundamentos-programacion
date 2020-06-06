6c83c1dc0f89a8525b4629d9420db44d656675fe
Guillermo Pizarro <gpizarro@ieee.org>
2020-06-06 11:59:18 -0500

    Programas utilizando estructuras de control-bucles.

---
+++ code/bucles_01.py
@@ -0,0 +1,11 @@
+for i in range(1,7):
+    for j in range(1, 11):
+        print("*", end='')
+    print("")
+
+print()
+
+for i in range(1,7):
+    for j in range(1, i+1):
+        print("*", end='')
+    print("")
